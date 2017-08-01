a96dd289cd55dff6173ed98cb3df16bb3ce8e21e
Daniel Duffield <danieljduffield@gmail.com>
2017-08-01 14:55:24 -0700

    Imported the getNextNoteId function from noteId in index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const notes = require('./utils/notes.js')
+const { getNextNoteId } = require('./noteId.js')
 
 app.use(express.static('server/public'))
 
@@ -8,6 +9,11 @@ app.get('/notes/', async (req, res) => {
   const noteList = await notes
   const jsonNotes = JSON.stringify(noteList)
   res.send(jsonNotes)
+})
+
+app.post('/note-submit-request/', (req, res) => {
+  const current = getNextNoteId()
+  res.send(current)
 })
 
 app.listen(3000, () => console.log('Listening on 3000...'))
