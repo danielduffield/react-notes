0b9718fa12df827811955fab24eb3ea3b979dd57
Daniel Duffield <danieljduffield@gmail.com>
2017-08-01 12:51:28 -0700

    Changed the fetch location to be at '/notes/' instead of '/'. #1

--- client/notesList.js
+++ client/notesList.js
@@ -1,32 +1,38 @@
 import React from 'react'
 
 export default class NotesList extends React.Component {
-  constructor(props, notes) {
+  constructor(props) {
     super(props)
     this.state = {notes: []}
   }
   async componentDidMount() {
     const notes = await getNotes()
+    console.log('notes', notes)
     this.setState({ notes })
   }
   render() {
     return (
-      this.state.notes.map((note, index) => {
-        return (
-          <div key={index}>
-            <span className="note-id">{note.id}</span>
-            <h3 className="note-title">{note.title}</h3>
-            <div className="note-container">
-              <p className="note-content">{note.content}</p>
-            </div>
-          </div>
-        )
-      })
+      <div className="note-container">
+        {this.state.notes
+          .map((note, index) => {
+            return (
+              <div className="note-container" key={index}>
+                <span className="note-id">{note.id}</span>
+                <h3 className="note-title">{note.title}</h3>
+                <div className="note-text-container">
+                  <p className="note-content">{note.content}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     )
   }
 }
 
 async function getNotes() {
-  const fetchedNotes = await fetch('http://localhost:3000')
-  return fetchedNotes
+  const fetchedNotes = await fetch('http://localhost:3000/notes/')
+  const notes = await fetchedNotes.json()
+  return notes
 }
