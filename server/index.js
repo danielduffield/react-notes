c8bf0c301c20ea569ffe1de9375d19a73d65af03
Daniel Duffield <danieljduffield@gmail.com>
2017-08-01 14:58:49 -0700

    Imported body parser and used the .json method to parse the request body into json. #2

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
 const express = require('express')
 const app = express()
-const notes = require('./utils/notes.js')
-const { getNextNoteId } = require('./noteId.js')
 
+const notes = require('./utils/notes.js')
+const { getNextNoteId, incrementNextId } = require('./noteId.js')
+
+app.use(jsonParser)
 app.use(express.static('server/public'))
 
 app.get('/notes/', async (req, res) => {
   const noteList = await notes
   const jsonNotes = JSON.stringify(noteList)
+
   res.send(jsonNotes)
 })
 
 app.post('/note-submit-request/', (req, res) => {
   const current = getNextNoteId()
+
+  incrementNextId()
   res.send(current)
 })
 
