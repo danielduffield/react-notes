0dcbec339ce44f8e88f71a9003c42cb939ef4442
Daniel Duffield <danieljduffield@gmail.com>
2017-08-02 13:26:01 -0700

    Pointed main.js to newly named notesComponent file.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import NotesComponent from './utils/notesList.js'
+import NotesComponent from './utils/notesComponent.js'
 
 render(<NotesComponent />, document.getElementById('note-list'))
