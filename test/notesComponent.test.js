1a560379e708cc0d0da503d0b99dbaa05880a31c
Daniel Duffield <danieljduffield@gmail.com>
2017-08-04 10:55:45 -0700

    Added a test for server POST requests. #7

--- test/notesComponent.test.js
+++ test/notesComponent.test.js
@@ -4,8 +4,8 @@ const request = require('request')
 
 describe('notes component containing the NotesList and NoteForm', () => {
 
-  describe('getNotes', () => {
-    it('sends a GET request to the server and receives the JSON notes', () => {
+  describe('server responses to GET requests at /notes/', () => {
+    it('responds with the JSON notes', () => {
       const fetchedNotes = requestNotes()
       fetchedNotes.then(notes => {
         expect(notes).to.be.an('array')
@@ -17,12 +17,38 @@ describe('notes component containing the NotesList and NoteForm', () => {
       })
     })
   })
+
+  describe('server response to POST request at /note-submit-request/', () => {
+    it('responds with an object corresponding to the latest note', () => {
+      const postedNote = submitNote()
+      postedNote.then(note => {
+        expect(note).to.be.an('object')
+        expect(note.content).to.equal('Mocha Test Note Content')
+      })
+    })
+  })
 })
 
 function requestNotes() {
   return new Promise((resolve, reject) => {
     request.get('http://localhost:3000/notes/', (err, response, body) => {
-      if (err) return reject(new Error('Did not successfully receive notes'))
+      if (err) return reject(new Error('Unable to acquire notes'))
+      return resolve(response)
+    })
+  })
+}
+
+function submitNote() {
+  return new Promise((resolve, reject) => {
+    request.post({
+      url: 'http://localhost:3000/note-submit-request',
+      noteData: {
+        title: 'Mocha Test Note Title',
+        content: 'Mocha Test Note Content'
+      }
+    },
+    (err, response, body) => {
+      if (err) return reject(new Error('Unable to submit a note'))
       return resolve(response)
     })
   })
